d831d716182cd814c993020381a4fd722383132a
BuildTools <unconfigured@null.spigotmc.org>
2019-11-17 14:06:33 -0600

    11/17/19

--- Lottery Simulator.py
+++ Lottery Simulator.py
@@ -1,6 +1,20 @@
 import random
 import math
 import threading
+import pynput
+
 
 def lottery_numbers():
-    num1 = random.randint(1,63)
+    while True:
+        num1 = random.randint(1,63)
+        num2 = random.randint(1,63)
+        num3 = random.randint(1,63)
+        num4 = random.randint(1,63)
+        num5 = random.randint(1,63)
+        powerball = random.randint(1,26)
+        print (num1,num2,num3,num4,num5,powerball)
+
+
+lottery_numbers()
+
+
