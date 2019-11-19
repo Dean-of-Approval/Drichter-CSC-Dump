3420d6415afe6ce0338b7333b20ec3f7be4eca92
BuildTools <unconfigured@null.spigotmc.org>
2019-11-19 12:10:46 -0600

    11/19/19

--- Lottery Simulator.py
+++ Lottery Simulator.py
@@ -4,17 +4,25 @@ import threading
 import pynput
 
 
-def lottery_numbers():
-    while True:
-        num1 = random.randint(1,63)
-        num2 = random.randint(1,63)
-        num3 = random.randint(1,63)
-        num4 = random.randint(1,63)
-        num5 = random.randint(1,63)
-        powerball = random.randint(1,26)
-        print (num1,num2,num3,num4,num5,powerball)
+def rand_lottery_numbers():
+    num1 = random.randint(1,69)
+    num2 = random.randint(1,69)
+    num3 = random.randint(1,69)
+    num4 = random.randint(1,69)
+    num5 = random.randint(1,69)
+    powerball = random.randint(1,26)
+    print ("Your numbers are:",num1,num2,num3,num4,num5,"and your powerball is:",powerball)
+
+def user_lottery_numbers():
+    num1 = int(input("Please select your first lottery Number:"))
+    num2 = int(input("Please select your second lottery Number:"))
+    num3 = int(input("Please select your third lottery Number:"))
+    num4 = int(input("Please select your fourth lottery Number:"))
+    num5 = int(input("Please select your fifth lottery Number:"))
+    powerball = int(input("Please select your powerball Number:"))
+    print ("Your numbers are:",num1,num2,num3,num4,num5,"and your powerball is:",powerball)
 
 
-lottery_numbers()
+user_lottery_numbers()
 
 
--- Mouse input.py
+++ Mouse input.py
@@ -1,13 +1,30 @@
+from time import sleep 
 from pynput.mouse import Listener
+from os import system, name 
+
+def clear(): 
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+  
 
 def on_move(x,y):
+    clear()
     print(x,y)
+    
 
 def on_click(x,y,button,pressed):
+    clear()
     print(x,y,button,pressed)
+    
 
 def on_scroll(x,y,dx,dy):
+    clear()
     print((x,y,dx,dy))
+    
 
 with Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
     listener.join()
