0b797444259a6cf80bec41b153d06518e1a69fb5
Drichter1075 <dean.richterent@gmail.com>
2019-11-19 13:48:50 -0600

    11/19/19

--- Lottery Simulator.py
+++ Lottery Simulator.py
@@ -1,7 +1,6 @@
 import random
 import math
 import threading
-import pynput
 
 
 def rand_lottery_numbers():
@@ -14,6 +13,8 @@ def rand_lottery_numbers():
     print ("Your numbers are:",num1,num2,num3,num4,num5,"and your powerball is:",powerball)
 
 def user_lottery_numbers():
+    print("Please select numbers between 1 and 69 inclusive for the lottery numbers")
+    print("and 1 to 26 inclusive for the powerball number")
     num1 = int(input("Please select your first lottery Number:"))
     num2 = int(input("Please select your second lottery Number:"))
     num3 = int(input("Please select your third lottery Number:"))
@@ -22,7 +23,21 @@ def user_lottery_numbers():
     powerball = int(input("Please select your powerball Number:"))
     print ("Your numbers are:",num1,num2,num3,num4,num5,"and your powerball is:",powerball)
 
+def powerball():
+    num11 = random.randint(1,69)
+    num21 = random.randint(1,69)
+    num31 = random.randint(1,69)
+    num41 = random.randint(1,69)
+    num51 = random.randint(1,69)
+    powerball1 = random.randint(1,26)
+
+def user_num_check(num1,num2,num3,num4,num5,powerball):
+    if num1<=69 and num2<=69 and num3<=69 and num4<=69 and num5<=69 and powerball<=26:
+        if num1>=1 and num2>=1 and num3>=1 and num4>=1 and num5>=1 and powerball>=1:
+            return True
+        else:
+            return False
+    else:
+        return False
 
 user_lottery_numbers()
-
-
