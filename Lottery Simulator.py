0f4c13fbf9f03c3611cda33c3c11a6429c869e4d
Drichter1075 <dean.richterent@gmail.com>
2019-12-06 15:26:18 -0600

    re worked program flow

--- Lottery Simulator.py
+++ Lottery Simulator.py
@@ -1,6 +1,7 @@
 import random
 import math
 import threading
+from os import system, name
 rand_correct = False
 """
 list of functions
@@ -10,18 +11,37 @@ list of functions
     rand_lottery_numbers(rand_correct)
 """
 
+def clear(): 
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
 
 def user_lottery_numbers():
-    print("Please select numbers between 1 and 69 inclusive for the lottery numbers")
-    print("and 1 to 26 inclusive for the powerball number")
-    num1 = int(input("Please select your first lottery Number:"))
-    num2 = int(input("Please select your second lottery Number:"))
-    num3 = int(input("Please select your third lottery Number:"))
-    num4 = int(input("Please select your fourth lottery Number:"))
-    num5 = int(input("Please select your fifth lottery Number:"))
-    powerball = int(input("Please select your powerball Number:"))
-    print ("Your numbers are:",num1,num2,num3,num4,num5,"and your powerball is:",powerball)
-    user_num_check(num1,num2,num3,num4,num5,powerball)
+    while user_num_test != True:
+        print("\033[1;37;40m Please select numbers between 1 and 69 inclusive for the lottery numbers")
+        print("and 1 to 26 inclusive for the powerball number")
+        num1 = int(input("Please select your first lottery Number:"))
+        num2 = int(input("Please select your second lottery Number:"))
+        num3 = int(input("Please select your third lottery Number:"))
+        num4 = int(input("Please select your fourth lottery Number:"))
+        num5 = int(input("Please select your fifth lottery Number:"))
+        powerball = int(input("Please select your powerball Number:"))
+        if num1<=69 and num2<=69 and num3<=69 and num4<=69 and num5<=69 and powerball<=26:
+            if num1>=1 and num2>=1 and num3>=1 and num4>=1 and num5>=1 and powerball>=1:
+                user_num_test = True
+            else:
+                clear()
+                print("\033[1;31;40m Please select numbers greater than 0")
+        else:
+            clear()
+            print("\033[1;31;40m Please select numbers less than 70 for the first 5 numbers and 27 for the powerball.")
+    
+    
+
+   
 
 def powerball():
     num11 = random.randint(1,69)
@@ -36,8 +56,12 @@ def user_num_check(num1,num2,num3,num4,num5,powerball):
         if num1>=1 and num2>=1 and num3>=1 and num4>=1 and num5>=1 and powerball>=1:
             return True
         else:
+            clear()
+            print("\033[1;31;40m Please select numbers greater than 0")
             user_lottery_numbers()
     else:
+        clear()
+        print("\033[1;31;40m Please select numbers less than 70 for the first 5 numbers and 27 for the powerball.")
         user_lottery_numbers()
 
 def rand_lottery_numbers(rand_correct):
@@ -53,3 +77,19 @@ def rand_lottery_numbers(rand_correct):
         else:
             rand_correct = False
     print ("Your numbers are:",num1,num2,num3,num4,num5,"and your powerball is:",powerball)
+
+userinput = input("Do you want random numbers? Y or N: ")
+
+if userinput.capitalize() == "Y":
+    rand_lottery_numbers(rand_correct)
+else:
+    user_lottery_numbers()
+
+
+
+
+
+
+print ("Your numbers are:",num1,num2,num3,num4,num5,"and your powerball is:",powerball)
+    user_satisified = input("Are you satisified with you number selections? Y or N")
+    return user_satisified
